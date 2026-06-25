8cfc6454fe5a8b1614d2d088ecaa0ebe64b0dbf3
tsiksdev-sketch <tsiksdev@gmail.com>
2026-06-25 14:36:35 +0200

    £

--- components/africarecs/africarecs.tsx
+++ components/africarecs/africarecs.tsx
@@ -139,7 +139,7 @@ const AfricaRECs = () => {
             <Link href="/start-a-diagnostic">Request REC Support <ArrowRight className="h-4 w-4" /></Link>
           </Button>
            <Button  className="mt-6 bg-emerald-500" asChild>
-            <Link href="https://africarecs.com/">If you want to register Visit  <ArrowRight className="h-4 w-4" /></Link>
+            <Link href="https://africarecs.com/">Visit our platform  <ArrowRight className="h-4 w-4" /></Link>
           </Button>
           </div>
          
